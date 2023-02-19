d288123ff1e64d3281cf15bffacaf5fbd08b19ea
ZUBR3784 <114622156+ZUBR3784@users.noreply.github.com>
2023-02-19 13:51:18 +0100

    HW2

--- алгоритмы/homeWorks/HW2/script.js
+++ алгоритмы/homeWorks/HW2/script.js
@@ -3,85 +3,76 @@
 // // если число N является точной степенью двойки, 
 // // или слово NO в противном случае.
 // // Операцией возведения в степень пользоваться нельзя!
-
-// let n = 36; 
-// const myNum = 2;
-// for (let i = 0; i<100; i++) { 
-//     if ( n==1) { 
-//     console.log('yes'); 
-//     break;
-//     } else if(n%myNum!==0){ 
-//     console.log('no');
-//     break;
-//     } else{ n=n/myNum} 
+// const myNum = 3; 
+// function square(n) { 
+//     if ( n===1) { 
+//         return 'yes' ; 
+//     } else if(n<1){ 
+//         return 'no'; 
+//     }  
+//     return square(n/myNum);   
 // }
-
-
+// console.log(square(1)); 
 
 
 // //  task -2
 // // Дано натуральное число N. Вычислите сумму его цифр.
 // // При решении этой задачи нельзя использовать строки, 
 // // списки, массивы (ну и циклы, разумеется).
-
-// let n = 179; 
-// let unit = n%10;
-// let t = (n - n % 10) / 10;
-// let decade = t%10
-// let hundred = (t-t%10)/10;
-// console.log(unit+decade+hundred);
-
-
+// function sumOfVal(myNum) {
+//     if (myNum<10) {
+//         return myNum
+//     }
+//     return myNum%10 + sumOfVal((myNum-(myNum%10))/10)
+// }
+// console.log(sumOfVal(1111111));
 
 
 // // task -3
 // // 2.1 Напишите рекурсивный метод, 
 // // который выводит на экран числа Фибоначчи до N-ого элемента.
+// let firstNum=0; 
+// let secNum=1; 
+// let sum=0;          
 
-// function fibon(n) { debugger
-//     let firstNum=0; debugger
-//     let secNum=1; debugger
-//     let sum=1; debugger
-//     for (let i = 0; i <= n; i++) {
-//         if (sum>n) {
-//             return console.log('end.');
-//         } else {
-//             console.log(sum); debugger
-//             firstNum=secNum; debugger
-//             secNum= sum; debugger
-//             sum= firstNum+secNum; debugger  
-//         }
+// function fibon(n) {  
+//     if (sum> n) { 
+//         return 'end'; 
+//     } else if(sum==n){
+//         return `${sum} end`; 
 //     }
-//    }
-//    fibon(5);
+//     else {
+//         firstNum=secNum; 
+//         secNum= sum; 
+//         sum= firstNum+secNum; 
+//         console.log(secNum);    
+//         return fibon(n);   
+//     }
+//   }
+//   console.log(fibon(55));
 
 
-
-// // function fibon(n) {                 //-Почему у меня так не получается?
-// //     if (n <= 0) {
-// //         return n
-// //     } else {
-// //         console.log(n);
-// //         return fibon(n - 1) + fibon(n - 2);
-// //     }
-// //   }
-// //   console.log( fibon(4) ); 
-
-
-
-
-// // task -4                                                   //вообще не понял где здесь рекурсия(
+// // task -4                                                  
 // // 2.2 Напишите рекурсивный метод, который проверяет, 
 // // является ли строка палиндромом.
 
-// let polindrom= "арозаупаланалапуазора";
-// // let polindrom = prompt("Введите слово")
-// function searchPolindrom(polindrom) {
-//     let polindrom2="";
-//     for (let i = polindrom.length-1; i>=0 ; i--) {
-//             polindrom2+=polindrom[i]; 
-//         }
-//         return (polindrom2 === polindrom)?alert(true):alert(false)
+// let polindrom= 'арозаупаланалапуазора';
+// let index =0; 
+// let lastInd=polindrom.length-1; 
+// function searchPolindrom(polindrom){
+//   if (index>=lastInd) { 
+//     return console.log('полиндром'); 
+//   }else if (polindrom[index]!==polindrom[lastInd]) { 
+//     return console.log('не полиндром'); 
+//   }else{
+//     index++;
+//     lastInd--; 
+//     return searchPolindrom(polindrom) 
+//   }
 // }
-// searchPolindrom(polindrom)
+// console.log(searchPolindrom(polindrom)); 
+
+
+
+
 
