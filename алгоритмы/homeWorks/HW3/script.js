e3359ffc50dbe83383f8250e18d4b458588bb0e6
ZUBR3784 <114622156+ZUBR3784@users.noreply.github.com>
2023-02-25 16:14:53 +0100

    HW3

---
+++ алгоритмы/homeWorks/HW3/script.js
@@ -0,0 +1,11 @@
+// // Решите задачу: Расставьте в алфавитном порядке буквы. Разрешается 
+// // использование техники Разделяй и властвуй. Полученные данные напечатайте.
+// // На вход строка: "poiuytrewqlkjhgfdsamnbvcxz"
+// // На выходе должно быть: ABCDEFGHIJKLMNOPQRSTUVWXYZ (с большой буквы)
+
+// const str='poiuytrewqlkjhgfdsamnbvcxz'.toLocaleUpperCase()
+// function alphabet(str) {
+//     return str.split('').sort().join('');
+//   }
+//   console.log(alphabet(str));
+
